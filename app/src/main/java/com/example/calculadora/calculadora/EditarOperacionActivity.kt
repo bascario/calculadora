7920343b7ace9aa91afdfea991a128c29cd4e979
Ruben <ruben.jaramillo@istvidanueva.edu.ec>
2025-11-29 11:50:31 -0500

    version casi acabada

--- app/src/main/java/com/example/calculadora/calculadora/EditarOperacionActivity.kt
+++ app/src/main/java/com/example/calculadora/calculadora/EditarOperacionActivity.kt
@@ -1,46 +1,51 @@
 package com.example.calculadora.calculadora
 
-import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.calculadora.calculadora.ui.theme.CalculadoraTheme
+import com.example.calculadora.calculadora.Database.CalculadoraDBHelper
+import com.example.calculadora.calculadora.Database.OperacionDAO
+import com.example.calculadora.calculadora.databinding.ActivityEditarOperacionBinding
 
 class EditarOperacionActivity : AppCompatActivity() {
     private lateinit var binding: ActivityEditarOperacionBinding
-    private lateinit var dao: OperacionDao
-    private var idOperacion=0
+    private lateinit var dao: OperacionDAO
+    private var idOperacion = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         binding = ActivityEditarOperacionBinding.inflate(layoutInflater)
         setContentView(binding.root)
         dao = OperacionDAO(this)
-        IdOperacion = intent.getIntExtra("id", 0)
+        idOperacion = intent.getIntExtra("id", 0)
         cargarDatos()
         binding.btnGuardar.setOnClickListener {
-            guardar()
+            guardarCambios()
         }
-
     }
 
     private fun cargarDatos() {
-        val helper = CalculadoraDbHelper(this)
-        val db: SQLiteDatabase=helper.readableDatabase
+        val helper = CalculadoraDBHelper(this)
+        val db = helper.readableDatabase
 
         val cursor = db.rawQuery(
             "SELECT descripcion, fecha FROM operaciones WHERE id=?",
             arrayOf(idOperacion.toString())
         )
+        if (cursor.moveToFirst()) {
+            binding.txtDescripcion.setText(cursor.getString(0))
+            binding.txtFecha.setText(cursor.getString(1))
+        }
         cursor.close()
         db.close()
     }
-    private fun guardarCambios(){
+
+    private fun guardarCambios() {
         val descripcion = binding.txtDescripcion.text.toString()
         val fecha = binding.txtFecha.text.toString()
-        if(descripcion.isBlank() || fecha.isBlank()) {
+        if (descripcion.isBlank() || fecha.isBlank()) {
             Toast.makeText(this, "Debe completar todos los campos", Toast.LENGTH_SHORT).show()
             return
         }
